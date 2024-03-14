9836db494316121e1a0b1b2edae670cf6be41ee1
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2024-03-14 22:43:10 +0800

    request

--- functions/api/[[submit]].js
+++ functions/api/[[submit]].js
@@ -3,36 +3,30 @@
  */
 export async function onRequest({ request }) {
 
-	async function gatherResponse(response) {
-		const { headers } = response;
-		const contentType = headers.get("content-type") || "";
-		if (contentType.includes("application/json")) {
-			return JSON.stringify(await response.json());
-		}
-		return await response.text();
-	}
+	// async function gatherResponse(response) {
+	// 	const { headers } = response;
+	// 	const contentType = headers.get("content-type") || "";
+	// 	if (contentType.includes("application/json")) {
+	// 		return JSON.stringify(await response.json());
+	// 	}
+	// 	return await response.text();
+	// }
 
-	const init = {
-		headers: {
-			"content-type": "application/json;charset=UTF-8",
-		},
-	};
+	// const init = {
+	// 	headers: {
+	// 		"content-type": "application/json;charset=UTF-8",
+	// 	},
+	// };
 
 	// return new Response("dddddddddddd")
 
 	try {
-		const url = "https://amd-api.magicaio.com/api/admin/login";
+		const reqUrl = request.url.split("/api")[1]
+		const url = "https://amd-api.magicaio.com/api/" + reqUrl;
 		const modifiedRequest = new Request(url, {
-			method: request.method, body: request.body, headers: {
-				...request.headers,
-				"Content-Type": "application/json;charset=UTF-8"
-			}
+			method: request.method, body: request.body, headers: request.headers
 		});
-		console.log(`[LOGGING FROM ${request.url}, ${JSON.stringify(modifiedRequest.method)}, ${modifiedRequest.body}`)
-
 		const response = await fetch(modifiedRequest);
-		// const results = await gatherResponse(response);
-		// return new Response(results, init);
 		return new Response(await response.text());
 	} catch (e) {
 		return new Response(JSON.stringify({ error: e.message }), {
