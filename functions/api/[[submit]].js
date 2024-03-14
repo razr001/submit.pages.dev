a01d5ace6805fa0f5a4633704729bf9ea5dc3fb7
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2024-03-14 20:51:28 +0800

    gatherResponse

--- functions/api/[[submit]].js
+++ functions/api/[[submit]].js
@@ -2,7 +2,25 @@
  * POST /api/submit
  */
 export async function onRequest({ request }) {
+
+	async function gatherResponse(response) {
+		const { headers } = response;
+		const contentType = headers.get("content-type") || "";
+		if (contentType.includes("application/json")) {
+			return JSON.stringify(await response.json());
+		}
+		return response.text();
+	}
+
+	const init = {
+		headers: {
+			"content-type": "application/json;charset=UTF-8",
+		},
+	};
+
 	const url = "https://amd-api.magicaio.com";
 	const modifiedRequest = new Request(url, request);
-	return await fetch(modifiedRequest);
+	const response  = await fetch(modifiedRequest);
+	const results = await gatherResponse(response);
+	return new Response(results, init);
 }
