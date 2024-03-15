5c8a9f902d8274109acaddcb94284e4aee931ac6
timou <razr001@gmail.com>
2024-03-15 16:54:32 +0800

    Update [[submit]].js

--- functions/api/[[submit]].js
+++ functions/api/[[submit]].js
@@ -2,38 +2,16 @@
  * POST /api/submit
  */
 export async function onRequest({ request }) {
-
-	async function gatherResponse(response) {
-		const { headers } = response;
-		const contentType = headers.get("content-type") || "";
-		if (contentType.includes("application/json")) {
-			return JSON.stringify(await response.json());
-		}
-		return await response.text();
-	}
-
-	const init = {
-		headers: {
-			"content-type": "application/json;charset=UTF-8",
-		},
-	};
-
-	// return new Response("dddddddddddd")
-
 	try {
 		const reqUrl = request.url.split("/api")[1]
 		const url = "https://amd-api.magicaio.com/api" + reqUrl;
 		const modifiedRequest = new Request(url, {
 			method: request.method, body: request.body, headers: request.headers
 		});
-		console.log(`[LOGGING FROM ${url}, ${JSON.stringify(modifiedRequest.method)}, ${modifiedRequest.body}`)
-
 		return await fetch(modifiedRequest);
 	} catch (e) {
 		return new Response(JSON.stringify({ error: e.message }), {
 			status: 500,
 		});
 	}
-
-
 }
